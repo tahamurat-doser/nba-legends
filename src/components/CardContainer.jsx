7fdf74c67f71b760db1cab5237534941e77f66af
taha_ murat <ttahamuratdoser@gmail.com>
2024-04-09 02:05:10 +0300

    cards added

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,5 +1,34 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { data } from "../helper/data";
+import Card from "react-bootstrap/Card";
 
+import Form from "react-bootstrap/Form";
 const CardContainer = () => {
-    return <div>NBA Legends</div>
-}
-export default CardContainer
+  console.log(data);
+  return (
+    <div>
+      {" "}
+      <Form.Control type="search" placeholder="Search legends..." />
+      <Container>
+        <Row xs={1} sm={1} md={2} lg={3} xl={4}>
+          {data.map((item) => (
+            <Col>
+              <Card>
+                <Card.Img variant="top" src={item.img} />
+                <Card.Body>
+                  <Card.Title className="cardTitle">{item.name}</Card.Title>
+                </Card.Body>
+                <ul className="m-auto">
+                    {
+                       item.statistics.map((ist) => <li className="list-unstyled h5 text-start">🏀{ist}</li> )
+                    }
+                </ul>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+export default CardContainer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Image from 'react-bootstrap/Image'
 import Logo from "../assets/nba-logo.png"
 const Header = () => {
     return <Container>
-        <Image src={Logo} width="200px"  rounded /><br/>
+        <Image src={Logo} width="300px"  rounded /><br/>
         <h2 className="title my-4">NBA LEGENDS</h2>
     </Container>
 }
